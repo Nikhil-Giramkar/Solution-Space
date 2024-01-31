2e58feb626ca6a7ee16f5d7a93e868d10fbd5af9
Nikhil Giramkar <niksgiramkar@gmail.com>
2024-01-31 10:04:25 +0530

    Middleware intro in ExpresJs

--- server/app.js
+++ server/app.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express();
 const PORT = 8765;
 
+
+//Middleware
+//Middlewares have access to Request, response and next() function, which help calling next middleware below it
+const middleware = (req, res, next) =>{
+    console.log(`Hello, my middeware called`);
+    next();
+}
+
 app.get("/", (req, res)=>{
     res.send("Hello World from Backend Home Page!!!");
 })
@@ -10,8 +18,10 @@ app.get("/contact", (req, res)=>{
     res.send("Hello World from Backend Contact Us!!!");
 })
 
-app.get("/about", (req, res)=>{
-    res.send("Hello World from Backend About Us!!!");
+//I will add middleware as second param here, for endpoint I wish to secure
+app.get("/about", middleware, (req, res)=>{
+    //The middleware will be called first, before serving this endpoint
+    res.send("Hello World from Backend About Me!!!");
 })
 
 app.get("/signin", (req, res)=>{
