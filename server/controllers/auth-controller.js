396142e7ad06ef6055c3c153d00bc5a7ec87c435
Nikhil Giramkar <niksgiramkar@gmail.com>
2024-02-03 23:16:17 +0530

    register router and logic seperated

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -5,8 +5,12 @@ In Express.js, controllers are used to process incoming request and interact wit
 and send response back to clients.
 The help organize aplication and seperating concerns, following MVC
 */
-//Home Logic
 
+//Get User model
+const User = require('../model/userSchema');
+
+
+//Home Logic
 const home = async(req, res) =>{
     try{
         res
@@ -19,4 +23,36 @@ const home = async(req, res) =>{
     }
 }
 
-module.exports = {home}
+//Register Logic
+const register = async (req, res) => {
+    //See what we get in request body
+    console.log(req.body);
+
+    //Object destructuring
+    const { name, email, phone, work, password, confirmPassword } = req.body;
+
+    //validation
+    if (!name || !email || !phone || !work || !password || !confirmPassword) {
+        return res.status(422).json({ error: "Please fill all fields" });
+    }
+    try {
+        //Check if user with same details already exists
+        const userExists = await User.findOne({ email: email })
+
+        if (userExists)
+            return res.status(422).json({ error: "User with same email exists" });
+
+        //If user is new
+        const user = new User({ name, email, phone, work, password, confirmPassword });
+
+        //waiting to save in Atlas
+        await user.save()
+        res.status(201).json({ message: "User registered successfully" })
+    }
+    catch (err) {
+        console.log(err);
+    }
+
+}
+
+module.exports = {home, register}
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -4,86 +4,11 @@ const router = express.Router();
 //Make DB Connection
 require('../db/conn');
 
-//Get User model
-const User = require('../model/userSchema');
+const authControllers = require('../controllers/auth-controller');
 
-const home = require('../controllers/auth-controller');
+router.route('/').get(authControllers.home);
 
-router.route('/').get(home);
-
-//Promises version
-// router.post('/register', (req, res) => {
-//     //See what we get in request body
-//     console.log(req.body);
-
-//     //Object destructuring
-//     const { name, email, phone, work, password, confirmPassword } = req.body;
-
-//     //validation
-//     if (!name || !email || !phone || !work || !password || !confirmPassword) {
-//         return res.status(422).json({ error: "Please fill all fields" });
-//     }
-
-//     //Check if user with same details already exists
-//     User.findOne({ email: email })
-//         .then((userExists) => {
-//             if (userExists)
-//                 return res.status(422).json({ error: "User with same email exists" });
-
-//             //If user is new
-//             //We can simply pass req.body as parameter, all fields will get set automatically
-//             //  const user = new User(req.body)
-//             //But preferred way is to send all needed fields manually, since we might not need, 1 or 2 fileds for DB
-//             const user = new User({ name, email, phone, work, password, confirmPassword });
-
-//             //the user object is like a document, or a row in table
-//             user.save() //To save to DB, also it returns a promise
-//                 .then(() => {
-//                     res.status(201).json({ message: "User registered successfully" })
-//                 })
-//                 .catch((err) => {
-//                     res.status(500).json({ error: "User could not be registered" })
-//                 })
-
-
-//         })
-//         .catch(err => {
-//             console.log(err);
-//         })
-// })
-
-
-//Async Await version
-router.route('/register').post(async (req, res) => {
-    //See what we get in request body
-    console.log(req.body);
-
-    //Object destructuring
-    const { name, email, phone, work, password, confirmPassword } = req.body;
-
-    //validation
-    if (!name || !email || !phone || !work || !password || !confirmPassword) {
-        return res.status(422).json({ error: "Please fill all fields" });
-    }
-    try {
-        //Check if user with same details already exists
-        const userExists = await User.findOne({ email: email })
-
-        if (userExists)
-            return res.status(422).json({ error: "User with same email exists" });
-
-        //If user is new
-        const user = new User({ name, email, phone, work, password, confirmPassword });
-
-        //waiting to save in Atlas
-        await user.save()
-        res.status(201).json({ message: "User registered successfully" })
-    }
-    catch (err) {
-        console.log(err);
-    }
-
-})
+router.route('/register').post(authControllers.register)
 
 
 //Sign In
