fab8858ff5b4a4600f2b7fe5774fa10a3215c035
Nikhil Giramkar <niksgiramkar@gmail.com>
2024-02-10 12:09:35 +0530

    Copied CSS files Put Nav inside container div

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,30 +8,32 @@ export const Navbar = () => {
                     <div className="logo-brand">
                         <NavLink to="/">Nikhil Logo</NavLink>
                     </div>
+
+                    <nav>
+                        <ul>
+                            <li>
+                                <NavLink to="/">Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/about">About</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/contact">Contact</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/sevice">Service</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/register">Register</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/login">Login</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+
                 </div>
 
-                <nav>
-                    <ul>
-                        <li>
-                            <NavLink to="/">Home</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/about">About</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/contact">Contact</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/sevice">Service</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/register">Register</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/login">Login</NavLink>
-                        </li>
-                    </ul>
-                </nav>
             </header>
         </>
     )
