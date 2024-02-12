895b266684e71094624273603046fe05f908c61a
Nikhil Giramkar <niksgiramkar@gmail.com>
2024-02-12 19:51:58 +0530

    Home Page UI

---
+++ client/src/components/Analytics.jsx
@@ -0,0 +1,29 @@
+export const Analytics = () => {
+    return (
+        <>
+            <section className="section-analytics">
+                <div className="container grid grid-four-cols">
+                    <div className="div1">
+                        <h2>50+</h2>
+                        <p>Registered Companies</p>
+                    </div>
+
+                    <div className="div1">
+                        <h2>10,000+</h2>
+                        <p>Happy Clients</p>
+                    </div>
+
+                    <div className="div1">
+                        <h2>500+</h2>
+                        <p>Well known Developers</p>
+                    </div>
+
+                    <div className="div1">
+                        <h2>24/7</h2>
+                        <p>Service</p>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
